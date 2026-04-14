8f3b9b7ea44447bec2cb54aacb7df35f16955599
Miryala Avinash Kumar <avinashkumarmiryala@gmail.com>
2026-04-15 00:30:10 +0530

    Edited Connection

--- utils/connection.py
+++ utils/connection.py
@@ -1,5 +1,5 @@
 import psycopg2
-"""
+
 def get_db_connection():
     try:
         conn = psycopg2.connect(
@@ -13,13 +13,4 @@ def get_db_connection():
     except Exception as e:
         print(f"Error connecting to database: {e}")
         return None
-"""
-def get_db_connection():
-    conn = psycopg2.connect(
-        host = 'localhost',
-        database = 'temporaldb',
-        user='postgres',
-        password='Avinash6174',
-        port='5432'
-    )
-    return conn
+
