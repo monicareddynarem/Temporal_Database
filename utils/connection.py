965ca46a75de8d38300b2d7ecbddf8d82126d3de
AshokChandraRamakurthi <ashokchandraramakurthi@kgpian.iitkgp.ac.in>
2026-04-14 18:16:33 +0530

    modified the index_vs_noindex

--- utils/connection.py
+++ utils/connection.py
@@ -3,10 +3,10 @@ import psycopg2
 def get_db_connection():
     try:
         conn = psycopg2.connect(
-            dbname="23CS10059",
-            user="23CS10059",
-            password="ashok@123",
-            host="10.5.18.102",
+            dbname="23CS10046",
+            user="23CS10046",
+            password="monica@2006",
+            host="10.5.18.101",
             port="5432"
         )
         return conn
