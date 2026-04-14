4b088e471f0515010636ffe533941a403eb7e79f
monicareddynarem <monicareddynarem@gmail.com>
2026-04-14 17:35:32 +0530

    changed many files

--- utils/connection.py
+++ utils/connection.py
@@ -1,19 +1,19 @@
 import psycopg2
 
-def get_db_connection():
-    try:
-        conn = psycopg2.connect(
-            dbname="23CS10059",
-            user="23CS10059",
-            password="ashok@123",
-            host="10.5.18.102",
-            port="5432"
-        )
-        return conn
-    except Exception as e:
-        print(f"Error connecting to database: {e}")
-        return None
-'''
+# def get_db_connection():
+#     try:
+#         conn = psycopg2.connect(
+#             dbname="23CS10059",
+#             user="23CS10059",
+#             password="ashok@123",
+#             host="10.5.18.102",
+#             port="5432"
+#         )
+#         return conn
+#     except Exception as e:
+#         print(f"Error connecting to database: {e}")
+#         return None
+
 def get_db_connection():
     conn = psycopg2.connect(
         host = 'localhost',
@@ -23,4 +23,3 @@ def get_db_connection():
         port='5432'
     )
     return conn
-'''
